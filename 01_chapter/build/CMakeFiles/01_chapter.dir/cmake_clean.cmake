bde352ab443d9143578cc99f62214b67785ecde7
Sibi-Ros-Coder <sibi001@e.ntu.edu.sg>
2022-01-19 22:14:07 +0800

    first time compiling multiple directories

---
+++ 01_chapter/build/CMakeFiles/01_chapter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "01_chapter"
+  "01_chapter.pdb"
+  "CMakeFiles/01_chapter.dir/src/first.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_chapter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 01_chapter/src/first.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+int main()
+{
+std::cout<<"that cliche Hello World"<<std::endl;
+}
+
